41c68499829ae243b6da5c70ce668e6078988d1b
“Ligui0711” <msklee0711@gmail.com>
2022-09-28 17:36:41 +0900

    更新処理routes記述&テストOK

--- testApp/routes/api.php
+++ testApp/routes/api.php
@@ -20,3 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('todo/create', [TodoController::class, 'store'])->name('api.todo.create');
+
+Route::post('todo/update', [TodoController::class, 'update'])->name('api.todo.update');
